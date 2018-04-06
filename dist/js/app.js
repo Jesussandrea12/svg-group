30a803b68d46ccf2a43707e01ce6555756218d90
jesus sandrea <jesussandrea12@gmail.com>
2018-04-06 18:17:06 -0400

    acercandome al design

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,3 +5,14 @@ $(document).ready(function(){
 		$(this).toggleClass('is-active');
 	});
 });
+
+$('.button').click(function(){
+  var buttonId = $(this).attr('id');
+  $('#modal-container').removeAttr('class').addClass(buttonId);
+  $('body').addClass('modal-active');
+})
+
+$('#modal-container').click(function(){
+  $(this).addClass('out');
+  $('body').removeClass('modal-active');
+});
